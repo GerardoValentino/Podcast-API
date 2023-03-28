64eb97e1608c7089d8ae9750be0b5e687817a893
GerardoValentino <gv.rosalesramos@ugto.mx>
2023-03-28 12:43:12 -0600

    Se agregaron los canales

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const cardTop = document.querySelector('#cardTop').content
+const contenido = document.querySelector('#contenido')
+const fragment = document.createDocumentFragment()
+const btnBuscar = document.getElementById('buscador')
+
+const cardCanal = document.querySelector('#cardChannel').content
+const fragment_canales = document.createDocumentFragment()
+const contenido_canales = document.querySelector('#contenido-canales')
+
 let categories = []
+let canales = []
 document.addEventListener('DOMContentLoaded', () => {
-    loadCategories();
+    loadCategories()
 })
 
 const loadCategories = () => {
@@ -11,30 +21,29 @@ const loadCategories = () => {
             'X-RapidAPI-Host': 'podcast-api1.p.rapidapi.com'
         }
     };
-    
+
     fetch('https://podcast-api1.p.rapidapi.com/categories', options)
         .then(response => response.json())
         .then(response => {
             categories = response.data
+            creaCards()
             console.log('Categorias', categories)
         })
-        .catch(err => console.error(err));
-} 
-
-
-
+        .catch(err => console.error(err))
+}
 
 // Funciones para el menu de carrusel
-function App() {}
-window.onload = function(event) {
+function App() { }
+window.onload = function (event) {
+    console.log('Entra a la funcion window.onload')
     var app = new App()
     window.app = app
 }
 
-App.prototype.processingButton = function(event) {
+App.prototype.processingButton = function (event) {
     const btn = event.currentTarget
     const carruselList = event.currentTarget.parentNode
-    const track = event.currentTarget.parentNode.querySelector('#track')
+    const track = event.currentTarget.parentNode.querySelector('.track')
     const carrusel = track.querySelectorAll('.carrusel')
     const carruselWidth = carrusel[0].offsetWidth
     const trackWidth = track.offsetWidth
@@ -54,3 +63,52 @@ let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => 
         track.style.left = `${-1 * (leftPosition + carruselWidth)}px`
     }
 }
+
+const creaCards = () => {
+    categories.forEach((item) => {
+        console.log(item)
+        cardTop.querySelector('img').setAttribute('src', item.image_url)
+        cardTop.querySelector('.category-name').textContent = item.name
+        cardTop.querySelector('button').setAttribute('value', item.id)
+        const clone = cardTop.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    contenido.appendChild(fragment)
+}
+
+const creaCanales = () => {
+    canales.forEach((item) => {
+        console.log(item)
+        cardCanal.querySelector('img').setAttribute('src', item.big_cover_url)
+        cardCanal.querySelector('.card-title').textContent = item.title
+        cardCanal.querySelector('.card-text').textContent = item.description
+        const clone = cardCanal.cloneNode(true)
+        fragment_canales.appendChild(clone)
+    })
+    contenido_canales.appendChild(fragment_canales)
+}
+
+
+btnBuscar.addEventListener('keyup', () => {
+    console.log('tecla', btnBuscar.value) // Cuando es un input es value; cuando es una etiqueta div, p... es textContent
+})
+
+function cargarCategoria(id) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '0e39151c7amsh86c23fd0ad07af8p1ebb07jsne2d4140fe256',
+            'X-RapidAPI-Host': 'podcast-api1.p.rapidapi.com'
+        }
+    };
+
+    fetch(`https://podcast-api1.p.rapidapi.com/top_channels/v2?category_id=${id}&country=us`, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log('Respuesta', response.data)
+            canales = response.data.list
+            creaCanales()
+        })
+        .catch(err => console.error(err));
+}
+
